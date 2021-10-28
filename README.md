1aed80d762865bcddca2867fb9b3c2a5f80f54b7
Manoj Kumar <manoganga1151@gmail.com>
2021-10-28 05:10:13 +0100

    sonar fixes

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # FizzBuzzChallenge
 This repository contains the Tests to validate FizzBuzz Conditions
 
-#SONAR CLOUD STATUS
+## SONAR CLOUD STATUS
 
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=manoj2507_FizzBuzzChallenge&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=manoj2507_FizzBuzzChallenge)
 
@@ -12,6 +12,17 @@ This repository contains the Tests to validate FizzBuzz Conditions
 [![Duplicated Lines (%)](https://sonarcloud.io/api/project_badges/measure?project=manoj2507_FizzBuzzChallenge&metric=duplicated_lines_density)](https://sonarcloud.io/summary/new_code?id=manoj2507_FizzBuzzChallenge)
 
 [![Security Rating](https://sonarcloud.io/api/project_badges/measure?project=manoj2507_FizzBuzzChallenge&metric=security_rating)](https://sonarcloud.io/summary/new_code?id=manoj2507_FizzBuzzChallenge)
+
+TestNG is used for Test Assertions and Test Reporting
+
+Run Configuration :
+Execute the test cases through Maven command - clean verify
+
+You can also run the test cases as TestNG either from the class or TestNG.xml
+
+Report :
+Emailable report is under target/surefire-reports/emailable-report.html
+index report is under target/surefire-reports/index.html
 
 
 
--- src/test/java/com/test/FizzBuzzTestNGTests.java
+++ src/test/java/com/test/FizzBuzzTestNGTests.java
@@ -20,30 +20,30 @@ public class FizzBuzzTestNGTests {
 
     @Test(description = "Validate FizzBuzz is returned for the number provided")
     public void fizzbuzz_shouldReturn_FIZZBUZZ_if_number_is_divisible_by_3_and_divisible_by_5() {
-        assertEquals(FizzBuzzConstants.FIZZBUZZ, fizzBuzz.evaluate(15));
-        assertEquals(FizzBuzzConstants.FIZZBUZZ, fizzBuzz.evaluate(30));
-        assertEquals(FizzBuzzConstants.FIZZBUZZ, fizzBuzz.evaluate(75));
+        assertEquals(fizzBuzz.evaluate(15),FizzBuzzConstants.FIZZBUZZ);
+        assertEquals(fizzBuzz.evaluate(30),FizzBuzzConstants.FIZZBUZZ);
+        assertEquals(fizzBuzz.evaluate(75),FizzBuzzConstants.FIZZBUZZ);
     }
 
     @Test(description = "Validate Fizz is returned for the number provided")
     public void fizzbuzz_shouldReturn_FIZZ_if_number_is_divisible_by_3() {
-        assertEquals(FizzBuzzConstants.FIZZ, fizzBuzz.evaluate(3));
-        assertEquals(FizzBuzzConstants.FIZZ, fizzBuzz.evaluate(36));
-        assertEquals(FizzBuzzConstants.FIZZ, fizzBuzz.evaluate(72));
+        assertEquals(fizzBuzz.evaluate(3),FizzBuzzConstants.FIZZ);
+        assertEquals(fizzBuzz.evaluate(36),FizzBuzzConstants.FIZZ);
+        assertEquals(fizzBuzz.evaluate(72),FizzBuzzConstants.FIZZ);
     }
 
     @Test(description = "Validate Buzz is returned for the number provided")
     public void fizzbuzz_shouldReturn_BUZZ_if_number_is_divisible_by_5() {
-        assertEquals(FizzBuzzConstants.BUZZ, fizzBuzz.evaluate(10));
-        assertEquals(FizzBuzzConstants.BUZZ, fizzBuzz.evaluate(40));
-        assertEquals(FizzBuzzConstants.BUZZ, fizzBuzz.evaluate(95));
+        assertEquals(fizzBuzz.evaluate(10),FizzBuzzConstants.BUZZ);
+        assertEquals(fizzBuzz.evaluate(40),FizzBuzzConstants.BUZZ);
+        assertEquals(fizzBuzz.evaluate(95),FizzBuzzConstants.BUZZ);
     }
 
     @Test(description = "Validate number is returned for the input")
     public void fizzbuzz_shouldReturn_Number_if_number_is_neither_divisible_by_3_or_5_or_both() {
-        assertEquals("1", fizzBuzz.evaluate(1));
-        assertEquals("43", fizzBuzz.evaluate(43));
-        assertEquals("91", fizzBuzz.evaluate(91));
+        assertEquals(fizzBuzz.evaluate(1),"1");
+        assertEquals(fizzBuzz.evaluate(43),"43");
+        assertEquals(fizzBuzz.evaluate(91),"91");
     }
 
 }
